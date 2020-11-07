e60b8913e8c6d7e2561a1a2cdf889de372e22734
YohanJabin <yohan.jabin@epitech.eu>
2020-11-07 15:00:56 +0100

    Upgrade project to unity 2020 and add Player sprite, animations and basic controller script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb;
+    public float movementSpeed = 1f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontalMovement * movementSpeed, rb.velocity.y);
+        animator.SetFloat("Horizontal", rb.velocity.x);
+    }
+}
