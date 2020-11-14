da5f5864f1ac69b3db3f32b9c9461eef9cbdae59
YohanJabin <yohan.jabin@epitech.eu>
2020-11-14 18:52:24 +0100

    Add windows and lights now blinks

---
+++ Assets/Scripts/RandomLight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class RandomLight : MonoBehaviour
+{
+    private float baseIntensity;
+    private Light2D light2d;
+    public float intensityRandomness = 0.1f;
+    private float timeBetweenRandomness = 0.05f;
+    private float timer;
+
+    void Start()
+    {
+        light2d = GetComponent<Light2D>();
+        baseIntensity = light2d.intensity;
+        timer = timeBetweenRandomness;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0f) {
+            if (Random.Range(0, 2) == 0) {
+                light2d.intensity = Random.Range(baseIntensity - intensityRandomness, baseIntensity + intensityRandomness + 0.001f);
+            }
+            timer = timeBetweenRandomness;
+        }
+    }
+}
