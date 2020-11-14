a4b084d4d05416408f605b670a8a133e21860568
Romain CASTEL <romain.castel@epitech.eu>
2020-11-14 18:22:41 +0100

    [ADD] Menu v1

---
+++ Assets/Scripts/Menu/Door.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Animator _anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseOver()
+    {
+        _anim.SetBool("Open", true);
+    }
+
+    private void OnMouseExit()
+    {
+        _anim.SetBool("Open", false);
+    }
+
+
+    private void OnMouseDown()
+    {
+        MenuManager.Leave();
+    }
+}
---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class MenuManager
+{
+    private static string GameScene = "SampleScene";
+    private static string MenuScene = "Menu";
+
+
+    public static void GoToScene()
+    {
+        SceneManager.LoadScene(GameScene);
+    }
+    public static void GoToMenu()
+    {
+        SceneManager.LoadScene(MenuScene);
+    }
+
+    public static void Leave()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+      Application.Quit();
+#endif
+    }
+}
