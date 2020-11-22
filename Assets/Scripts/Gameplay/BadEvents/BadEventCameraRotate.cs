53d97ae5a1bf5595015a8cc7d58c9f68f880bd6c
EPLE Grégory <gregory.eple@epitech.eu>
2020-11-22 15:51:11 +0100

    Added camera zoom and shake depending on stress

---
+++ Assets/Scripts/Gameplay/BadEvents/BadEventCameraRotate.cs
@@ -0,0 +1,29 @@
+﻿using Cinemachine;
+using Player;
+using UnityEngine;
+
+namespace Gameplay.BadEvents
+{
+    public class BadEventCameraRotate : ABadEvent {
+
+        [SerializeField] private PlayerConstitution constitution = null;
+        [SerializeField] private CinemachineVirtualCamera _camera = null;
+        [SerializeField] private float minRotation = 0f;
+        [SerializeField] private float maxRotation = 1f;
+        private CinemachineBasicMultiChannelPerlin noise = null;
+
+        void Awake() {
+            noise = _camera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        }
+
+        private void Update() {
+            _camera.m_Lens.Dutch = minRotation + (maxRotation - minRotation) * (constitution.stress.Ratio / PlayerCharacteristic.Limit);
+            noise.m_AmplitudeGain = minRotation + (maxRotation - minRotation) * (constitution.stress.Ratio / PlayerCharacteristic.Limit);
+        }
+
+        private void OnDestroy() {
+            noise.m_FrequencyGain = 0;
+            noise.m_AmplitudeGain = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/BadEvents/BadEventCameraShake.cs
@@ -0,0 +1,29 @@
+﻿using Cinemachine;
+using Player;
+using UnityEngine;
+
+namespace Gameplay.BadEvents
+{
+    public class BadEventCameraShake : ABadEvent {
+
+        [SerializeField] private PlayerConstitution constitution = null;
+        [SerializeField] private CinemachineVirtualCamera _camera = null;
+        [SerializeField] private float minShake = 0f;
+        [SerializeField] private float maxShake = 1f;
+        private CinemachineBasicMultiChannelPerlin noise = null;
+
+        void Awake() {
+            noise = _camera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        }
+
+        private void Update() {
+            noise.m_FrequencyGain = minShake + (maxShake - minShake) * (constitution.stress.Ratio / PlayerCharacteristic.Limit);
+            noise.m_AmplitudeGain = minShake + (maxShake - minShake) * (constitution.stress.Ratio / PlayerCharacteristic.Limit);
+        }
+
+        private void OnDestroy() {
+            noise.m_FrequencyGain = 0;
+            noise.m_AmplitudeGain = 0;
+        }
+    }
+}
