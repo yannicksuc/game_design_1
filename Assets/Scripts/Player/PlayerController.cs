9392ab59039e25edbf7636c47e0b400c67ef7020
YohanJabin <yohan.jabin@epitech.eu>
2020-11-26 12:22:04 +0100

    Integrate breathing scene to gameplay scene, sync player breathing animation with breathing system (Not sure if it is a good idea)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Player;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private PlayerConstitution constitution;
     private Animator animator;
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
     public float movementSpeed = 1f;
     public float jumpSpeed = 5f;
 
@@ -13,16 +16,29 @@ public class PlayerController : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        OnBreathingStepChanged();
     }
 
     void Update()
     {
+        constitution.breath.StepChange.AddListener(OnBreathingStepChanged);
+
         if (Input.GetKeyDown(KeyCode.Space)) {
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
         }
 
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rb.velocity.y);
         animator.SetFloat("Horizontal", rb.velocity.x);
-        //animator.SetFloat("Vertical", rb.velocity.y);
+    }
+
+    private void OnBreathingStepChanged()
+    {
+        if (constitution.breath.Step == BreathStep.Inhale) {
+            animator.SetFloat("BreathStep", 0.1f);
+        }
+        else if (constitution.breath.Step == BreathStep.Exhale) {
+            animator.SetFloat("BreathStep", -0.1f);
+        }
     }
 }
